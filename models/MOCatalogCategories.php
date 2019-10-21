7ad6246820d43c806c028c5c55a666d40d1fe1fe
pavel <s20305382@gmail.com>
2019-10-21 23:03:42 +0300

    +

--- models/MOCatalogCategories.php
+++ models/MOCatalogCategories.php
@@ -7,7 +7,7 @@ namespace megaoffice\client\models;
 class MOCatalogs
 {
     public static function getAll($condition = null){
-        $res = \Yii::$app->megaofficeClient->query('/catalogs', $condition);
+        $res = \Yii::$app->megaofficeClient->query('/catalogs/categories', $condition);
         return $res;
     }
 }
