d30cc6eef6477986760bf005343a9bbb2cdd5f46
pavel <s20305382@gmail.com>
2019-12-18 02:15:04 +0200

    Работоспособность сигнализации вебсокет

--- models/MOCatalogCategories.php
+++ models/MOCatalogCategories.php
@@ -4,20 +4,18 @@
 namespace megaoffice\client\models;
 
 
-class MOClients extends MOActiveRecord
+class MOCatalogCategories extends MOActiveRecord
 {
     public static function getAll($condition = null){
         $res = \Yii::$app->megaofficeClient->query('/catalog/categories', $condition);
         return $res;
     }
 
-
     public static $tableName = 'catalog/categories';
 
     public static $attrList = [
         'id', 'catalog_id', 'parent_id', 'name','descr','disabled',  'options',
     ];
-
 
     public static function tableName(){
         return static::$tableName;
