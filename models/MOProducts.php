e9add019228f8b5328fb14368ea55814a0cb24b6
pavel <s20305382@gmail.com>
2020-02-06 04:43:59 +0200

    Комплектация

--- models/MOProducts.php
+++ models/MOProducts.php
@@ -3,13 +3,32 @@
 
 namespace megaoffice\client\models;
 
-
+/**
+ * Class MOProducts
+ * @package megaoffice\client\models
+ *
+ * @property integer $id;
+ * @property integer $category_id;
+ * @property integer $type_id;
+ * @property array $options;
+ */
 class MOProducts
 {
     public static function getAll($condition = null){
         $res = \Yii::$app->megaofficeClient->query('/catalog/products', $condition);
         return $res;
     }
+
+    public static $tableName = 'catalog/products';
+
+    public static $attrList = [
+        'id', 'catalog_id', 'type_id', 'options',
+    ];
+
+    public static function tableName(){
+        return static::$tableName;
+    }
+
 
 
 }
